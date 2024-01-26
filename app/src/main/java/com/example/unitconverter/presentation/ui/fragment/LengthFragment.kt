ef113739c7db264063a81db1e7e3d9b3fc184f9c
Jubayed-A <learnigprograming@gmail.com>
2024-01-26 09:51:40 +0600

    LengthFragment.kt updated

--- app/src/main/java/com/example/unitconverter/presentation/ui/fragment/LengthFragment.kt
+++ app/src/main/java/com/example/unitconverter/presentation/ui/fragment/LengthFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.unitconverter.R
 
-class AboutFragment : Fragment() {
+class LengthFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        return inflater.inflate(R.layout.fragment_length, container, false)
     }
 
 }
