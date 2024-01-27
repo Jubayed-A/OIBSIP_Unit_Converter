2ca53b2111a9ab3c012226b8dcb5b5cc625ceb21
Jubayed-A <learnigprograming@gmail.com>
2024-01-27 22:20:48 +0600

    file added

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,10 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
+buildscript {
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10") // Update to the latest version
+        classpath ("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.22-1.0.16")
+    }
+}
