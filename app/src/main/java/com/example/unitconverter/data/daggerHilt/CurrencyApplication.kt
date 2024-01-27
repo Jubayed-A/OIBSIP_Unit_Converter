86a4efe256835de8fc3b99e704a676a8ac0d34e1
Jubayed-A <learnigprograming@gmail.com>
2024-01-27 22:23:26 +0600

    file added

---
+++ app/src/main/java/com/example/unitconverter/data/daggerHilt/CurrencyApplication.kt
@@ -0,0 +1,7 @@
+package com.example.unitconverter.data.daggerHilt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CurrencyApplication : Application()
