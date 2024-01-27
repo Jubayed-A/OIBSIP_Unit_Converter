7d68812400fbe922236ab44ea532fdb445ec8404
Jubayed-A <learnigprograming@gmail.com>
2024-01-27 22:23:13 +0600

    file added

---
+++ app/src/main/java/com/example/unitconverter/data/CurrencyApi.kt
@@ -0,0 +1,16 @@
+package com.example.unitconverter.data
+
+import com.example.unitconverter.data.models.CurrencyResponse
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CurrencyApi {
+
+    @GET("/latest")
+    suspend fun getRates(
+        @Query("base") base: String
+    ): Response<CurrencyResponse>
+
+}
