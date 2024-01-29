f27eaea7ee93af10a00717393c1f7f8ad7467b3e
Jubayed-A <learnigprograming@gmail.com>
2024-01-29 10:02:08 +0600

    LengthFragment.kt file updated

--- app/src/main/java/com/example/unitconverter/presentation/ui/fragment/LengthFragment.kt
+++ app/src/main/java/com/example/unitconverter/presentation/ui/fragment/LengthFragment.kt
@@ -1,20 +1,55 @@
 package com.example.unitconverter.presentation.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
 import com.example.unitconverter.R
+import com.example.unitconverter.databinding.FragmentLengthBinding
 
 class LengthFragment : Fragment() {
+
+    private lateinit var binding: FragmentLengthBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_length, container, false)
+        binding = FragmentLengthBinding.inflate(layoutInflater, container, false)
+
+        // time conversion section
+        val fromMenu = resources.getStringArray(R.array.lengthSection)
+        val toMenu = resources.getStringArray(R.array.lengthSection)
+
+        // from menu drop down section
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, fromMenu)
+        binding.fromSection.setAdapter(arrayAdapter)
+        binding.fromSection.setOnItemClickListener { parent, view, position, id ->
+            val selectedOption = parent.getItemAtPosition(position).toString()
+            //Toast.makeText(requireContext(), "You Selected $selectedOption", Toast.LENGTH_SHORT).show()
+            binding.fromSectionText.text = selectedOption
+        }
+
+        // to menu drop down section
+        val arrayAdapter2 = ArrayAdapter(requireContext(), R.layout.drop_down_item, toMenu)
+        binding.toSection.setAdapter(arrayAdapter2)
+        binding.toSection.setOnItemClickListener { parent, view, position, id ->
+            val selectedOption = parent.getItemAtPosition(position).toString()
+            //Toast.makeText(requireContext(), "You Selected $selectedOption", Toast.LENGTH_SHORT).show()
+            binding.toSectionText.text = selectedOption
+        }
+
+        binding.lengthConvert.setOnClickListener {
+            convertLength()
+        }
+
+        return binding.root
+    }
+
+    private fun convertLength() {
+        TODO("Not yet implemented")
     }
 
 }
