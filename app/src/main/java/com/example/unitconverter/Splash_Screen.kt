4ba55b2b14bae0a03df77337bfd70e4934c1a2af
Jubayed-A <learnigprograming@gmail.com>
2024-01-30 07:02:35 +0600

    dark mode and white mode status bar color file updated for splash screen

--- app/src/main/java/com/example/unitconverter/Splash_Screen.kt
+++ app/src/main/java/com/example/unitconverter/Splash_Screen.kt
@@ -1,14 +1,33 @@
 package com.example.unitconverter
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
 
 class Splash_Screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        // Set the default night mode for the entire application to follow the system setting
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+
+        // Check if the current night mode is night mode
+        val isNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+
+        // Set the status bar color
+        if (isNightMode) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.darkStatusBar)
+        } else {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.whiteStatusBar)
+            // Adjust system UI visibility flags for light status bar icons
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
 
         Handler().postDelayed({
             startActivity(Intent(this,MainActivity::class.java))
